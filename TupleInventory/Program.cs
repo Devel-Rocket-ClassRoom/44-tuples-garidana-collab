323481a5405b08820bc8744a6d38f2a14b6a56cc
garidana-collab <garidana@gmail.com>
2026-03-12 01:37:46 +0900

    44 Tuple Inv in progress

--- TupleInventory/Program.cs
+++ TupleInventory/Program.cs
@@ -1,4 +1,33 @@
 using System;
-
+using System.Collections.Generic;
+using static System.Console;
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+
+
+List<(string name, int quantity, int price)> InvList;
+
+void AddItem (List<(string name, int quantity, int price)> inventory, string name, int quantity, int price)
+{
+    var Item = (name, quantity, price);
+    inventory.Add(Item);
+    InvList = inventory;
+}
+
+(string name, int price) FindMostExpensive (List<(string name, int quantity, int price)> inventory)
+{
+    var maxPrice = 0;
+    var maxName = "";
+    foreach (var item in inventory)
+    {
+        
+        if (maxPrice > item.price)
+        {
+            maxPrice = item.price;
+            maxName = item.name;
+        }
+    }
+    return (maxName, maxPrice);
+}
+
+AddItem( "체력 포션", 3, 500);
